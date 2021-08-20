2e33253c4c3ac7cec920bba101c2cab8bf8bd2eb
Johnathan Etchart <johnathan.etchart@gmail.com>
2021-08-19 19:42:04 -0700

    basic express server and added users to schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE `Sessions` (
 	`phase_id` int NOT NULL,
 	`mesocycle_id` int NOT NULL,
 	`microcycle_id` int NOT NULL,
+	`user_id` int NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
@@ -13,6 +14,7 @@ CREATE TABLE `Sets` (
 	`load` int NOT NULL,
 	`reps` int NOT NULL,
 	`session_id` int NOT NULL,
+	`user_id` bigint NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
@@ -22,6 +24,7 @@ CREATE TABLE `Microcycle` (
 	`deload` BOOLEAN NOT NULL AUTO_INCREMENT,
 	`mesocycle_id` int NOT NULL,
 	`phase_id` int NOT NULL,
+	`user_id` int NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
@@ -29,12 +32,21 @@ CREATE TABLE `Mesocycle` (
 	`id` int NOT NULL AUTO_INCREMENT,
 	`date` DATETIME NOT NULL,
 	`phase_id` int NOT NULL,
+	`user_id` int NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `Phase` (
 	`id` int NOT NULL AUTO_INCREMENT,
 	`date` DATETIME NOT NULL,
+	`user_id` DATETIME NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Users` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL AUTO_INCREMENT,
+	`weight` int(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
@@ -44,13 +56,24 @@ ALTER TABLE `Sessions` ADD CONSTRAINT `Sessions_fk1` FOREIGN KEY (`mesocycle_id`
 
 ALTER TABLE `Sessions` ADD CONSTRAINT `Sessions_fk2` FOREIGN KEY (`microcycle_id`) REFERENCES `Microcycle`(`id`);
 
+ALTER TABLE `Sessions` ADD CONSTRAINT `Sessions_fk3` FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`);
+
 ALTER TABLE `Sets` ADD CONSTRAINT `Sets_fk0` FOREIGN KEY (`session_id`) REFERENCES `Sessions`(`id`);
+
+ALTER TABLE `Sets` ADD CONSTRAINT `Sets_fk1` FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`);
 
 ALTER TABLE `Microcycle` ADD CONSTRAINT `Microcycle_fk0` FOREIGN KEY (`mesocycle_id`) REFERENCES `Mesocycle`(`id`);
 
 ALTER TABLE `Microcycle` ADD CONSTRAINT `Microcycle_fk1` FOREIGN KEY (`phase_id`) REFERENCES `Phase`(`id`);
 
+ALTER TABLE `Microcycle` ADD CONSTRAINT `Microcycle_fk2` FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`);
+
 ALTER TABLE `Mesocycle` ADD CONSTRAINT `Mesocycle_fk0` FOREIGN KEY (`phase_id`) REFERENCES `Phase`(`id`);
+
+ALTER TABLE `Mesocycle` ADD CONSTRAINT `Mesocycle_fk1` FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`);
+
+ALTER TABLE `Phase` ADD CONSTRAINT `Phase_fk0` FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`);
+
 
 
 
