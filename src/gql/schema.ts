6b168e1ea54b68ba70260296bed07fcb6c49de4a
Omar Masri <marismaro@gmail.com>
2022-05-25 20:04:42 -0700

    GraphQL Initial Uptake (#2)
    * initial commit
    
    * unnecessary dep

---
+++ src/gql/schema.ts
@@ -0,0 +1,62 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type User {
+    id: Int!
+    weight: Int!
+    name: String!
+  }
+
+  type Phase {
+    id: Int!
+    date: String!
+    user_id: Int!
+    name: String!
+  }
+
+  type Mesocycle {
+    id: Int
+    date: String!
+    phase_id: Int!
+    user_id: Int!
+  }
+
+  type Microcycle {
+    id: Int!
+    date: String!
+    deload: Boolean
+    mesocycle_id: Int!
+    phase_id: Int!
+    user_id: Int!
+  }
+
+  type Session {
+    id: Int!
+    date: String!
+    name: String!
+    microcycle_id: Int!
+    mesocycle_id: Int!
+    phase_id: Int!
+    user_id: Int!
+  }
+
+  type Set {
+    id: Int!
+    load: Int!
+    reps: Int!
+    session_id: Int!
+    user_id: Int!
+  }
+
+  type Query {
+    user(id: Int!): User
+    allUsers(limit: Int, offset: Int): [User]
+  }
+
+  type Mutation {
+    createUser(name: String!, weight: Int!): User!
+    updateUser(id: Int!, name: String!, weight: Int!): User!
+  }
+`;
+
+module.exports = typeDefs;
